bcbcc33171850dedfa4e52ca82b1df1834a0df8e
YoJames2019 <yobiscuit0@gmail.com>
2026-04-14 02:33:42 -0400

    update nyaasi extension to work

--- nyaasi.js
+++ nyaasi.js
@@ -1,38 +1,68 @@
-export default new class Nyaa {
-  base = 'https://torrent-search-api-livid.vercel.app/api/nyaasi/'
+export default new class ApiClient {
+  async single({ titles, episode }, options) {
+    if(options.apiUrl === "") {
+      throw new Error("You must specify the base url of the third party nyaa.si api you are using in settings\n\nExample (not functional): https://nyaasi.yourwebsite.net")
+    }
 
-  async single({ titles, episode }) {
     if (!titles?.length) return []
-    return this.search(titles[0], episode)
+
+    const query = this.buildSearchQuery(titles[0], episode)
+    const headers = {
+      "Content-Type": "application/json",
+    }
+
+    if(options.apiKey !== ""){
+      headers["X-API-Key"] = options.apiKey
+    }
+
+    const res = await fetch(`${options.apiUrl}/api/search`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({ term: query }),
+    });
+
+    if (!res.ok) {
+      if(res.status === 429){
+        if(options.apiKey !== "") {
+          throw new Error("Invalid or incorrect API key!")
+        }
+        throw new Error("You cannot access this api without authorization! If you have an API key, make sure to put it in the extension settings!")
+      }
+
+      return []
+    };
+    
+    const data = await res.json()
+
+    if (!Array.isArray(data)) return []
+
+    return this.map(data)
   }
 
   batch = this.single
   movie = this.single
 
-  async search(title, episode) {
+  buildSearchQuery(title, episode) {
     let query = title.replace(/[^\w\s-]/g, ' ').trim()
     if (episode) query += ` ${episode.toString().padStart(2, '0')}`
-
-    const res = await fetch(this.base + encodeURIComponent(query))
-    const data = await res.json()
-    if (!Array.isArray(data)) return []
-
-    return data.map(item => ({
-      title: item.Name,
-      link: item.Magnet,
-      hash: item.Magnet?.match(/btih:([A-Fa-f0-9]+)/)?.[1] || '',
-      seeders: Number(item.Seeders || 0),
-      leechers: Number(item.Leechers || 0),
-      downloads: Number(item.Downloads || 0),
-      size: 0,
-      date: new Date(item.DateUploaded),
-      accuracy: 'medium',
-      type: 'alt'
-    }))
+    return query
   }
 
+  map(data) {
+    return data.map(item => ({
+      title: item.name || '',
+      link: item.magnet || '',
+      seeders: parseInt(item.seeders || '0'),
+      leechers: parseInt(item.leechers || '0'),
+      downloads: parseInt(item.completed || '0'),
+      accuracy: 'medium',
+      hash: item.hash || '',
+      size: item.filesize,
+      date: new Date(item.date)
+    }))
+  }
+  
   async test() {
-    const res = await fetch(this.base + 'one%20piece')
-    return res.ok
+    return true
   }
 }()
