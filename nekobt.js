0b3db70ca2e89507e5fcc4ff2b4bf13ebe57511f
YoJames2019 <yobiscuit0@gmail.com>
2026-05-20 13:33:21 -0400

    nekobt new ver that isnt shit

--- nekobt.js
+++ nekobt.js
@@ -1,74 +1,61 @@
-
-const CUSTOM_EPOCH = BigInt("1735689600000");
-
-function decodeId(encodedId) {
-  const id = BigInt(encodedId);
-  const time = Number((id >> 8n) + CUSTOM_EPOCH);
-  const type = Number((id >> 4n) & 15n);
-  const increment = Number(id & 15n);
-  return { time, type, increment };
-}
-
 export default new class NekoBT {
-    url = atob("aHR0cHM6Ly9uZWtvYnQudG8vYXBpL3YxLw==");
+  url = atob("aHR0cHM6Ly9uZWtvYnQudG8vYXBpL3YxLw==");
 
-    async fetchEpisodeFromId({ tvdbId, tmdbId, imdbId }) {
-        const mappingRes = await fetch(
-            atob("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1RoYVVua25vd24vYW5pbWUtbGlzdHMtdHMvcmVmcy9oZWFkcy9tYWluL2RhdGEvbmJ0LW1hcHBpbmcuanNvbg==")
-        )
-        console.log(`tvdbid: ${tvdbId}, tmdbid: ${tmdbId}, imdbid: ${imdbId}`)
-        const mappings = await mappingRes.json()
+  async _fetch(fetch, search) {
+    const res = await fetch(`${this.url}torrents/search?${search}`), json = await res.json();
+    if (json.error) throw new Error("NekoBT: " + json.message);
+    if (!json.data) throw new Error("NekoBT: Invalid response from server!");
+    return json.data;
+  }
 
-        const nekoID = mappings.tvdb[tvdbId] ?? mappings.tmdb[tmdbId] ?? mappings.imdb[imdbId];
-        if (!nekoID) return {};
-        const res = await fetch(`${this.url}media/${nekoID}`)
-        
-        const json = await res.json();
-        if (json.error) throw new Error("NekoBT: " + json.message);
-        return { nekoID, data: json.data };
-    }
+  async single({tvdbId: tvdbId, tvdbEId: tvdbEId, tmdbId: tmdbId, episode: episode, fetch: fetch}, options) {
+    if (!navigator.onLine) return [];
+
+    const mediaParams = new URLSearchParams({
+      limit: "1"
+    });
+
+    if(tvdbId) mediaParams.append("tvdbid", tvdbId.toString());
+    if(tmdbId) mediaParams.append("tmdbid", tmdbId);
+
+    const mappings = await this._fetch(fetch, mediaParams);
+    if (!mappings?.media) throw new Error("NekoBT: No media found for the given anime!");
+
+    const ep = mappings.media.episodes?.find(ep => ep.tvdbId === tvdbEId) ?? mappings.media.episodes?.find(ep => ep.episode === episode), searchParams = new URLSearchParams({
+      media_id: mappings.media.id,
+      fansub_lang: "en,enm",
+      sub_lang: "en,enm"
+    });
+
+    if(ep?.id) searchParams.append("episode_ids", ep.id.toString());
+
+    const high = ep?.tvdbId === tvdbEId;
     
-    mapResults(res) {
-        return (
-            res?.data?.results?.map((result) => ({
-                title: result.title,
-                link: `${this.url}torrents/${result.id}/download?public=true`,
-                seeders: Number(result.seeders),
-                leechers: Number(result.leechers),
-                downloads: Number(result.completed),
-                hash: result.infohash,
-                size: Number(result.filesize),
-                accuracy: "high",
-                type: (result.level ?? 0) >= 3 ? "alt" : void 0,
-                date: new Date(decodeId(result.id).time),
-            })) ?? []
-        );
-    }
+    return (await this._fetch(fetch, searchParams)).results?.map(entry => ({
+      title: entry.title,
+      link: `${this.url}torrents/${entry.id}/download?public=true`,
+      seeders: Number(entry.seeders),
+      leechers: Number(entry.leechers),
+      downloads: Number(entry.completed),
+      hash: entry.infohash,
+      size: Number(entry.filesize),
+      accuracy: high ? "high" : "medium",
+      type: (entry.level ?? 0) >= 3 ? "alt" : entry.batch ? "batch" : void 0,
+      date: new Date(entry.uploaded_at)
+    })) ?? [];
+  }
 
-    async single({ tvdbId, tvdbEId, tmdbId, imdbId, episode }, i) {
-        if (!navigator.onLine) return [];
-        if (!tvdbId && !tmdbId && imdbId) return []
+  batch=() => [];
 
-        const nekoMapping = await this.fetchEpisodeFromId({ tvdbId, tmdbId, imdbId })
-        if(!nekoMapping) return [];
-
-        const episodeData = nekoMapping.data?.episodes?.find((e) => e.tvdbId === tvdbEId) ?? nekoMapping.data?.episodes?.find((e) => e.episode === episode);
-        let URL = `${this.url}torrents/search?media_id=${nekoMapping.nekoID}&fansub_lang=en%2Cenm&sub_lang=en%2Cenm`;
-        episodeData?.id && (URL += `&episode_ids=${episodeData.id}`);
-        const res = await fetch(URL)
-        
-        const json = await res.json();
-        if (json.error) throw new Error("NekoBT: " + json.message);
-        return this.mapResults(json);
+  movie=() => [];
+  
+  async test() {
+    try {
+      const {ok: ok} = await fetch(this.url + "announcements");
+      if (!ok) throw new Error(`Failed to load data from ${this.url}! Is the site down?`);
+      return !0;
+    } catch (error) {
+      throw new Error(`Could not reach ${this.url}! Does the site work in your region?`);
     }
-    batch = this.single;
-    movie = this.single;
-    async test() {
-        try {
-            if (!(await fetch(this.url + "announcements")).ok) throw new Error(`Failed to load data from ${this.url}! Is the site down?`);
-            return true;
-        } catch (err) {
-            throw new Error(`Could not reach ${this.url}! Does the site work in your region?`);
-        }
-    }
-}();
+  }
+};
